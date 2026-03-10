c32f8d012b9ac97e36f90f1f6cbfe85e4678bdbf
SamithISE <vvce23ise0219@vvce.ac.in>
2026-03-10 13:03:56 +0530

    Update smithaa.java

--- smithaa.java
+++ smithaa.java
@@ -1,5 +1,7 @@
 public class smithaa{
 public static void main(String [] args){
 System.out.println("Hi smithaaaaaaaaaaaaaa");
+System.out.println("Hi smithaaaaaaaaaaaaaa how r uuuuuuuu");
+  
 }
 }
